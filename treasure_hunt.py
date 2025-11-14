3955051e954415f3689a40601df3d10d38d84a2b
laxitpdl <poudellakshit042061@gmail.com>
2025-11-13 19:53:53 -0600

    If_else

--- treasure_hunt.py
+++ treasure_hunt.py
@@ -6,4 +6,23 @@ if height >= 120:
     
 else:
     print("Sorry safety issue! ")
+    
+    
+
+
+#modulo operator %     : remainder
+
+# EVEN - ODD!
+number = int(input("Enter any whole number: "))
+even = number % 2. # if remainder = 0
+
+if even == 0:
+    print ("EVEN")
+else:
+    print("ODD")    
+    
+    
+    
+
+
         
