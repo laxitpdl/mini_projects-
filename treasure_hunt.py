5c36822e3b7e20412fc42c8aaccc14304385535d
laxitpdl <poudellakshit042061@gmail.com>
2025-11-13 20:05:44 -0600

    if-else

--- treasure_hunt.py
+++ treasure_hunt.py
@@ -1,9 +1,14 @@
 print("Welcome to roller-coaster")
 height = int(input("Enter your height in cm: "))
-
+age = int(input("Enter your age: "))
 if height >= 120:
-    print("You can have fun! ")
+    print("Have fun! ")
     
+    if age <= 18:
+        print("You are charged $7!")
+    else:
+        print ("You are charged $14!")    
+         
 else:
     print("Sorry safety issue! ")
     
@@ -20,6 +25,8 @@ if even == 0:
     print ("EVEN")
 else:
     print("ODD")    
+    
+    
     
     
     
