d0e1c288beb2ae4fe86bc03538400223935f8d7b
laxitpdl <poudellakshit042061@gmail.com>
2025-11-13 19:53:42 -0600

    if_else_

---
+++ treasure_hunt.py
@@ -0,0 +1,9 @@
+print("Welcome to roller-coaster")
+height = int(input("Enter your height in cm: "))
+
+if height >= 120:
+    print("You can have fun! ")
+    
+else:
+    print("Sorry safety issue! ")
+        
