0c93891fed63b22b040f6b840e1222303db08f0c
laxitpdl <poudellakshit042061@gmail.com>
2025-11-28 22:16:35 -0600

    ok

--- hangman.py
+++ hangman.py
@@ -5,20 +5,21 @@ words = ["abra", "kaoe", "gusdhhu"]
 guess = random.choice(words)
 print(guess)
 
+new_guess = len(guess)
+
+holder = ""
+for i in range(new_guess):
+    holder+="_"
+print(holder)
+
+display = ""
 user = input("Guess a letter: ").lower()
 for i in guess:
-    if user == i:
-        print("right")
-    else:
-        print("wrong")    
+   if user == i:
+       display+= i
+   else:
+       display += "_"
+       
+print(display)       
     
-    
-    
-    
-    #print(i)
-    
-    
-    
-    #if user == guess(user):
-        #print("right")
 
